b48c3b55b46f55a10184cd1eecfb47ae2b5062b2
mylongcode <mylongcode@gmail.com>
2024-09-19 21:20:46 +0500

    fix bugs

--- optimal_cutting_backend/optimal_cutting_backend/Program.cs
+++ optimal_cutting_backend/optimal_cutting_backend/Program.cs
@@ -14,11 +14,8 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(options =>
 {
-
-    var basePath = AppContext.BaseDirectory;
-
-    var xmlPath = Path.Combine(basePath, "vega.xml");
-    options.IncludeXmlComments(xmlPath);
+    var xmlFilename = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    options.IncludeXmlComments(Path.Combine(AppContext.BaseDirectory, xmlFilename));
 });
 
 var app = builder.Build();
