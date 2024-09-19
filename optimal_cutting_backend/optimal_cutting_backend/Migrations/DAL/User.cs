9ac4798d6154ff24aeaa87cea83e8138df44a1a0
mylongcode <mylongcode@gmail.com>
2024-09-19 13:19:18 +0500

    add models and connect db

---
+++ optimal_cutting_backend/optimal_cutting_backend/Migrations/DAL/User.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace vega.Migrations.DAL
+{
+    [Table("users")]
+    public class User
+    {
+        [Column("user_id")]
+        public int Id { get; set; }
+        [Column("login")]
+        public string Login { get; set; }
+        [Column("full_name")]
+        public string FullName { get; set; }
+        [Column("password")]
+        public string Password { get; set; }
+    }
+}
--- optimal_cutting_backend/optimal_cutting_backend/Program.cs
+++ optimal_cutting_backend/optimal_cutting_backend/Program.cs
@@ -1,14 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using vega.Migrations.EF;
+
 var builder = WebApplication.CreateBuilder(args);
 
-
+var connectionString = builder.Configuration.GetConnectionString("VegaDB");
+builder.Services.AddDbContext<VegaContext>(options =>
+    options.UseNpgsql(connectionString));
 
 builder.Services.AddControllers();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+    
 var app = builder.Build();
 
+app.UseCors(builder =>
+    builder.WithOrigins("http://localhost:3000")
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials()
+    );
 
 if (app.Environment.IsDevelopment())
 {
