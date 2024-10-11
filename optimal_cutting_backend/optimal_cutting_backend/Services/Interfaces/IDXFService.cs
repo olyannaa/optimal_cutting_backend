632c53222b1b788a3ac7bc583fd9faa1feeb75e2
mylongcode <mylongcode@gmail.com>
2024-10-11 20:33:25 +0500

    edit algorithm for draw dxf file

--- optimal_cutting_backend/optimal_cutting_backend/Services/Interfaces/IDXFService.cs
+++ optimal_cutting_backend/optimal_cutting_backend/Services/Interfaces/IDXFService.cs
@@ -5,6 +5,5 @@ namespace vega.Services.Interfaces
     public interface IDXFService
     {
         public List<FigureDTO> GetDXF(byte[] fileBytes);
-        public byte[] DrawPng(List<FigureDTO> figures);
     }
 }
